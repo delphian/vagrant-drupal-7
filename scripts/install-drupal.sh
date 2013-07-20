c6af71756056f5c9d5ec4642acd53c22efc50907
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-20 10:22:40 -0700

    Script running inside virtual machine creates database first.

--- scripts/install-drupal.sh
+++ scripts/install-drupal.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+mysql -h localhost -uroot -proot -e "create database drupal;"
 drush dl -v -d --destination=".." --drupal-project-rename="$(basename `pwd`)"
 drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:root@localhost/drupal
