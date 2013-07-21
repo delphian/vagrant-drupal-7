337670170e0abc2a001b37eb65f25147fed9eba8
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-21 05:57:59 -0700

    Removing requirement to hit yes when installing drupal.

--- scripts/install-drupal.sh
+++ scripts/install-drupal.sh
@@ -2,7 +2,7 @@
 mysql -h localhost -uroot -proot -e "create database drupal;"
 cd /var/www/local.drupal.org
 sudo drush dl -v -d --destination=".." --drupal-project-rename="$(basename `pwd`)" -y
-drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:root@localhost/drupal
+drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:root@localhost/drupal -y
 drush dl devel
 drush en devel -y
 drush dis overlay -y
