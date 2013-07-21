c6cf04191e742d1b5fb911c08a80f2156bb46ee5
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-20 22:02:30 -0700

    Changing directory in virtual machine before installing drupal.

--- scripts/install-drupal.sh
+++ scripts/install-drupal.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 mysql -h localhost -uroot -proot -e "create database drupal;"
+cd /var/www/local.drupal.org
 drush dl -v -d --destination=".." --drupal-project-rename="$(basename `pwd`)"
 drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:root@localhost/drupal
+drush dl devel
+drush en devel -y
+drush dis overlay -y
