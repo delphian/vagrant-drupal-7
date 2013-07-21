3af5b47fb6af7f625282af8ed685568c8672642b
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-21 09:39:48 -0700

    Fixing bad directory name.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,13 +2,14 @@
  
 # Executing the following line in the shell will automatically
 # download and install https://github.com/delphian/vagrant-drupal-7:
-# curl -s https://raw.github.com/delphian/vagrant-drupal-7/master/scripts/bootstrap.sh
+# curl -s https://raw.github.com/delphian/vagrant-drupal-7/master/scripts/bootstrap.sh | bash
 
 echo ""
-echo "\033[0;31mGist bootstrap is installing drupal 7 on virtual box with vagrant...\033[0m"
+echo "Bootstrap is installing drupal 7 on virtual box with vagrant..."
 echo "Cloning repository https://github.com/delphian/vagrant-drupal-7..."
 echo ""
 git clone --recursive git://github.com/delphian/vagrant-drupal-7.git
-cd vagrant-drupal/scripts
+cd vagrant-drupal-7/scripts
 ./install.sh
-echo "\033[0;31mGist bootstrap has finished installation.\033[0m"
+echo "Bootstrap has finished installation."
+echo ""
