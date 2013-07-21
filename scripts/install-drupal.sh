13bc15ac5c7c4088a453d7108aa5c0dfc80cf050
Bryan Hazelbaker <bryan.hazelbaker@gmail.com>
2013-07-20 22:55:20 -0700

    Fixing install drupal script.

--- scripts/install-drupal.sh
+++ scripts/install-drupal.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 mysql -h localhost -uroot -proot -e "create database drupal;"
 cd /var/www/local.drupal.org
-drush dl -v -d --destination=".." --drupal-project-rename="$(basename `pwd`)"
+sudo drush dl -v -d --destination=".." --drupal-project-rename="$(basename `pwd`)" -y
 drush site-install standard --account-name=admin --account-pass=admin --db-url=mysql://root:root@localhost/drupal
 drush dl devel
 drush en devel -y
